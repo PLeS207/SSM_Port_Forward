ab2618c60777aa7a0609c2d75b85a3d9a88319f2
Serhii Plesovskykh <47846911+PLeS207@users.noreply.github.com>
2023-08-07 15:43:59 +0300

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -7,8 +7,9 @@ echo $INSTANCE_ID
 ENDPOINT=$(aws rds describe-db-instances --query "DBInstances[?TagList[?Key=='Environment' && Value=='dev']].Endpoint.Address" --output text)
 echo $ENDPOINT
 aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"portNumber\":[\"1433\"],\"localPortNumber\":[\"1433\"],\"host\":[\"$ENDPOINT\"]}" &
-aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"portNumber\":[\"6379\"],\"localPortNumber\":[\"6379\"],\"host\":[\"master.xxxx\"]}" &
-aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"portNumber\":[\"9096\"],\"localPortNumber\":[\"9096\"],\"host\":[\"b-1.dev.xxxx\"]}" &
+aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"portNumber\":[\"6379\"],\"localPortNumber\":[\"6379\"],\"host\":[\"master.dev-cluster.nog7rh.euc1.cache.amazonaws.com:6379\"]}" &
+aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"portNumber\":[\"9096\"],\"localPortNumber\":[\"9096\"],\"host\":[\"b-1.dev.v30wqb.c8.kafka.eu-central-1.amazonaws.com\"]}" &
+aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"portNumber\":[\"22\"],\"localPortNumber\":[\"22\"],\"host\":[\"sftp-df.reeco.io\"]}" &
 
 export AWS_PROFILE=reeco-prod
 INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:k8s.io/cluster-autoscaler/enabled,Values=true" --query "Reservations[*].Instances[*].InstanceId" --output text | head -1)
@@ -16,5 +17,5 @@ echo $INSTANCE_ID
 ENDPOINT=$(aws rds describe-db-instances --query "DBInstances[?TagList[?Key=='Environment' && Value=='prod']].Endpoint.Address" --output text)
 echo $ENDPOINT
 aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"portNumber\":[\"1433\"],\"localPortNumber\":[\"1434\"],\"host\":[\"$ENDPOINT\"]}" &
-aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"portNumber\":[\"6379\"],\"localPortNumber\":[\"6380\"],\"host\":[\"master.xxxx\"]}" &
-aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"portNumber\":[\"9096\"],\"localPortNumber\":[\"9097\"],\"host\":[\"b-1.prod.xxxx\"]}" &
+aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"portNumber\":[\"6379\"],\"localPortNumber\":[\"6380\"],\"host\":[\"master.prod-cluster.rb4dlk.use1.cache.amazonaws.com:6379\"]}" &
+aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"portNumber\":[\"9096\"],\"localPortNumber\":[\"9097\"],\"host\":[\"b-2.prod.rmgdct.c3.kafka.us-east-1.amazonaws.com\"]}" &
